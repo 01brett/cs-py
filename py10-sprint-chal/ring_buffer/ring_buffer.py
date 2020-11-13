6328d659a6c0d446d80013a3223009d4374dad3e
brett f <1brettf@gmail.com>
2020-11-13 14:59:14 -0800

    Implement ring buffer

--- py10-sprint-chal/ring_buffer/ring_buffer.py
+++ py10-sprint-chal/ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
+from typing import ItemsView
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.store = []
+        self.cur = -1
 
     def append(self, item):
-        pass
+        # at capacity
+        if len(self.store) == self.capacity:
+            self.cur = (self.cur + 1) % self.capacity
+            self.store[self.cur] = item
+        # under capacity
+        else:
+            self.store.append(item)
+            self.cur += 1
 
     def get(self):
-        pass
+        return self.store
