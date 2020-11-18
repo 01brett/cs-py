702cf4f0343e8591f78857dd5b1ec473226b7d3f
brett f <1brettf@gmail.com>
2020-11-17 19:36:24 -0800

    Implement recursive algos

--- py12-recursive-sort/src/searching/searching.py
+++ py12-recursive-sort/src/searching/searching.py
@@ -1,14 +1,52 @@
-# TO-DO: Implement a recursive implementation of binary search
+# TO-DO: Implement a recursive binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # base case if our array is []
+    if end >= start:
+        middle = (start + end) // 2  # floor division aka round down
+
+        if target == arr[middle]:
+            return middle
+        elif target > arr[middle]:
+            # shift start pointer to right of middle
+            # effectively ignores left side of arr
+            start = middle + 1
+        else:
+            # shift end pointer to left of middle
+            # effectively ignores right side of arr
+            end = middle - 1
+        # recursive work
+        return binary_search(arr, target, start, end)
+
+    return -1  # not found
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    start = 0
+    end = len(arr) - 1
 
+    isAscending = arr[start] < arr[end]
+
+    while start <= end:
+        mid = (start + end) // 2  # floor division aka round down
+
+        if target == arr[mid]:
+            return mid
+
+        if isAscending:
+            if target > arr[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        else:
+            if target > arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+
+    return -1  # not found
--- py12-recursive-sort/src/sorting/sorting.py
+++ py12-recursive-sort/src/sorting/sorting.py
@@ -1,28 +1,104 @@
+# break down the arr into smaller chunks using a `pivot`
+# helper function to partition the input array
+def partition(arr):
+    # pick a pivot (1st el in this case)
+    pivot = arr[0]
+    left = []
+    right = []
+
+    # partition the elements around the pivot
+    for el in arr[1:]:
+        if el <= pivot:
+            left.append(el)
+        else:
+            right.append(el)
+
+    # we have el partitioned in left, pivot, and right arrays
+    return left, pivot, right
+
+
+def quicksort(arr):
+    # base case
+    if len(arr) <= 1:
+        return arr
+
+    # call partition() to breakup our inputs
+    left, pivot, right = partition(arr)  # Python destructuring
+
+    # concatenates our arrays together
+    # [pivot] just places our pivot val into an arr to aid arr concatenation
+    return quicksort(left) + [pivot] + quicksort(right)
+
+
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = []
+    # use AND b/c as soon as an arr is empty, then we just append the other /sorted/ arr to the list
+    while len(arrA) >= 1 and len(arrB) >= 1:
+        if arrA[0] <= arrB[0]:
+            merged_arr.append(arrA.pop(0))
+        else:
+            merged_arr.append(arrB.pop(0))
 
-    # Your code here
+    return merged_arr + arrA + arrB
 
-
-    return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    # base case
+    if len(arr) < 2:  # arr of 1 is sorted
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]  # excludes mid element
+    right = arr[mid:]  # includes mid element
+
+    return merge(merge_sort(left), merge_sort(right))
 
 
-    return arr
-
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
-    # Your code here
+    start2 = mid + 1
+
+    # If the direct merge is already sorted
+    if arr[mid] <= arr[start2]:
+        return
+
+    # Two pointers to maintain start
+    # of both arrays to merge
+    while start <= mid and start2 <= end:
+
+        # If element 1 is in right place
+        if arr[start] <= arr[start2]:
+            start += 1
+        else:
+            value = arr[start2]
+            index = start2
+
+            # Shift all the elements between element 1
+            # element 2, right by 1.
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
+
+            arr[start] = value
+
+            # Update all the pointers
+            start += 1
+            mid += 1
+            start2 += 1
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
+    if l < r:  # arr of 1 is sorted
+        # Same as (l + r) / 2, but avoids overflow for large l and r
+        mid = l + (r - l) // 2
 
+        # Sort first and second halves
+        merge_sort_in_place(arr, l, mid)
+        merge_sort_in_place(arr, mid + 1, r)
+
+        merge_in_place(arr, l, mid, r)
