44c0c3ff013ab1d2734a9f81b839aebf50f25014
brett f <1brettf@gmail.com>
2020-11-19 19:54:20 -0800

    Improve single num

--- py13_14-solutions/single_number/single_number.py
+++ py13_14-solutions/single_number/single_number.py
@@ -4,21 +4,21 @@ Returns: an integer
 """
 
 
-def single_number(arr):
-    # store the visited counts
-    count = {}
+# def single_number(arr):
+#     # store the visited counts
+#     cache = {}
 
-    # loop through arr counting what we see
-    for num in arr:
-        if num not in count:
-            count[num] = 1
-        else:
-            count[num] += 1
+#     # loop through arr counting what we see
+#     for num in arr:
+#         if num not in cache:
+#             cache[num] = 1
+#         else:
+#             cache[num] += 1
 
-    # return the single num
-    for num in count:
-        if count[num] == 1:
-            return num
+#     # return the single num
+#     for num in cache:
+#         if cache[num] == 1:
+#             return num
 
 
 # def single_number(arr):
@@ -35,6 +35,18 @@ def single_number(arr):
 
 #     # return the single num
 #     return seen[0]
+
+
+def single_number(arr):
+    ans = set()
+
+    for num in arr:
+        if num not in ans:
+            ans.add(num)
+        else:
+            ans.remove(num)
+
+    return list(ans)[0]
 
 
 if __name__ == "__main__":
