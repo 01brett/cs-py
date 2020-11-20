669ada242a3a23c32939c1510a03c33965eb1bb3
brett f <1brettf@gmail.com>
2020-11-19 20:41:24 -0800

    idk

--- py13_14-solutions/making_change/making_change.py
+++ py13_14-solutions/making_change/making_change.py
@@ -2,18 +2,39 @@
 
 import sys
 
-def making_change(amount, denominations):
-  # Your code here
+cache = {}
 
-  pass
+
+def making_change(amount, denominations):
+    # base case
+    if amount < 0:
+        return 0
+    elif amount == 0:
+        return 1
+    elif amount in cache:
+        return cache[amount]
+    else:
+        x = (
+            making_change(amount - 1)
+            + making_change(amount - 5)
+            + making_change(amount - 10)
+            + making_change(amount - 25)
+            + making_change(amount - 50)
+        )
+        cache[amount] = x
+        return x
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print(
+            "There are {ways} ways to make {amount} cents.".format(
+                ways=making_change(amount, denominations), amount=amount
+            )
+        )
+    else:
+        print("Usage: making_change.py [amount]")
--- py13_14-solutions/rock_paper_scissors/rps.py
+++ py13_14-solutions/rock_paper_scissors/rps.py
@@ -2,15 +2,26 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  # Your code here
 
-  pass
+def rock_paper_scissors(n):
+    if n == 0:
+        return [[]]
+
+    ans = []
+    plays = ["rock", "paper", "scissors"]
+
+    def inner(n):
+        if n == 0:
+            ans.append([])
+
+    inner(n)
+
+    return ans
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print("Usage: rps.py [num_plays]")
