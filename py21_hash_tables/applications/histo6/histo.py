e5b22998b86e687785624a7506e5b6bf7f265f6e
brett f <1brettf@gmail.com>
2020-11-25 15:53:17 -0800

    Implement histo

--- py21_hash_tables/applications/histo6/histo.py
+++ py21_hash_tables/applications/histo6/histo.py
@@ -1,2 +1,37 @@
-# Your code here
+import re
 
+
+def histo(s):
+    with open(f"histo6/{s}") as f:
+        words = f.read().split()
+
+    count = {}
+    longest_word = 0
+
+    for word in words:
+        # clean out extra junk
+        w = re.sub(r"[\"\:\;\,\.\-\+\=\/\\\|\[\]\{\}\(\)\*\^\&\?\!]", "", word).lower()
+
+        if w == "":
+            continue
+
+        if w not in count:
+            count[w] = 0
+
+        count[w] += 1
+
+        if len(w) > longest_word:
+            longest_word = len(w)
+
+    arr = list(count.items())
+    arr.sort(key=lambda el: (-el[1], el[0]))
+
+    for el in arr:
+        word = el[0]
+        size = el[1]
+        if size >= 6:
+            space = longest_word - len(word)
+            print(f"{word}{' ' * space}  {'#' * size}")
+
+
+histo("robin.txt")
