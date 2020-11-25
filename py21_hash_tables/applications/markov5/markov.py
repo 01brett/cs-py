68329a3b020ade1fd15f679fb02b7a26c07696ae
brett f <1brettf@gmail.com>
2020-11-25 15:12:30 -0800

    Implement markov chain

--- py21_hash_tables/applications/markov5/markov.py
+++ py21_hash_tables/applications/markov5/markov.py
@@ -1,13 +1,35 @@
 import random
+import re
 
 # Read in all the words in one go
-with open("input.txt") as f:
-    words = f.read()
+with open("markov5/input.txt") as f:
+    words = f.read().split()
 
-# TODO: analyze which words can follow other words
-# Your code here
+store = {}
+starts = []
 
 
-# TODO: construct 5 random sentences
-# Your code here
+# analyze which words can follow other words
+for i in range(len(words) - 1):
+    word = words[i]
+    next_word = words[i + 1]
 
+    if word not in store:
+        store[word] = []
+
+    store[word].append(next_word)
+
+    if re.match('^[A-Z]|^\"[A-Z]]', word):
+        starts.append(word)
+
+# construct 5 random sentences
+
+for i in range(5):
+    w = random.choice(starts)
+    print(w, end=" ")
+
+    while re.search('[\.\?\!]\"$|[\.\?\!]$', w) is None:
+        w = random.choice(store[w])
+        print(w, end=" ")
+
+    print("\n")
