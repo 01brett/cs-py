a50cea5599e3c1309aab11e2803d25168f2d553b
brett f <1brettf@gmail.com>
2020-11-25 14:00:55 -0800

    Implement word_count

--- py21_hash_tables/applications/word_count3/word_count.py
+++ py21_hash_tables/applications/word_count3/word_count.py
@@ -1,10 +1,32 @@
-def word_count(s):
-    # Your code here
+import re
 
+
+def word_count(s):
+    count = {}
+    if s == "":
+        return count
+    # split the words out from sentence
+    words = s.split()
+    # loop over split words array
+    for word in words:
+        # clean out extra junk
+        w = re.sub(r"[\"\:\;\,\.\-\+\=\/\\\|\[\]\{\}\(\)\*\^\&]", "", word).lower()
+        if w == "":
+            continue
+        if w not in count:
+            count[w] = 0
+
+        count[w] += 1
+
+    return count
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(
+        word_count(
+            "This is a test of the emergency broadcast network. This is only a test."
+        )
+    )
