802bb46d60f49ac6fdd93267e03ac1632d540501
brett f <1brettf@gmail.com>
2020-11-25 13:14:19 -0800

    Finish expensive_seq

--- py21_hash_tables/applications/expensive_seq2/expensive_seq.py
+++ py21_hash_tables/applications/expensive_seq2/expensive_seq.py
@@ -1,14 +1,26 @@
-# Your code here
-
-
 def expensive_seq(x, y, z):
-    # Your code here
+    cache = {}
 
+    def recurse(x, y, z):
+        if x <= 0:
+            return y + z
+
+        key = (x, y, z)
+
+        if key not in cache:
+            cache[key] = (
+                recurse(x - 1, y + 1, z)
+                + recurse(x - 2, y + 2, z * 2)
+                + recurse(x - 3, y + 3, z * 3)
+            )
+        return cache[key]
+
+    return recurse(x, y, z)
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
