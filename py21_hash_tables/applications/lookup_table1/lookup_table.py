d60f726632ae879c0b7b90a0363e825687daaba9
brett f <1brettf@gmail.com>
2020-11-25 13:03:18 -0800

    Implement lookup_table

--- py21_hash_tables/applications/lookup_table1/lookup_table.py
+++ py21_hash_tables/applications/lookup_table1/lookup_table.py
@@ -1,21 +1,30 @@
-# Your code here
+import math
+import random
 
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
-    v //= (x + y)
+    v //= x + y
     v %= 982451653
 
     return v
+
+
+cache = {}
+
 
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    key = f"{x},{y}"
 
+    if key not in cache:
+        cache[key] = slowfun_too_slow(x, y)
+
+    return cache[key]
 
 
 # Do not modify below this line!
@@ -23,4 +32,4 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f"{i}: {x},{y}: {slowfun(x, y)}")
