5ab28632bba050bef85defb4be3a473a8bb268ee
brett f <1brettf@gmail.com>
2020-11-25 14:10:41 -0800

    Implement no_dups

--- py21_hash_tables/applications/no_dups4/no_dups.py
+++ py21_hash_tables/applications/no_dups4/no_dups.py
@@ -1,6 +1,21 @@
 def no_dups(s):
-    # Your code here
+    if s == "":
+        return s
 
+    words = s.split()
+
+    if len(words) < 2:
+        return s
+
+    dedup = []
+    seen = {}
+
+    for word in words:
+        if word not in seen:
+            seen[word] = True
+            dedup.append(word)
+
+    return " ".join(dedup)
 
 
 if __name__ == "__main__":
