fb190089731c0aade5b3b5f769707148b41f10f4
brett f <1brettf@gmail.com>
2020-11-26 11:44:18 -0800

    Understand sumdiff

--- py21_hash_tables/applications/sumdiff8/sumdiff.py
+++ py21_hash_tables/applications/sumdiff8/sumdiff.py
@@ -3,13 +3,46 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
+q1 = (1, 3, 4, 7, 12)
+q2 = set(range(1, 10))
+# q3 = set(range(1, 200))
 
 
 def f(x):
     return x * 4 + 6
 
-# Your code here
 
+def sumdiff(q):
+    seen = {}
+    lookup = {n: f(n) for n in q}
+
+    def fn(src, arr):
+        combo = tuple(arr)
+        if combo in seen:  # return if we've seen combo
+            return
+
+        if len(arr) == 4:
+            seen[combo] = True  # mark this combo in cache
+            a, b, c, d = combo  # pull out refs from combo
+            ra, rb, rc, rd = (
+                lookup[a],
+                lookup[b],
+                lookup[c],
+                lookup[d],
+            )  # grab refs from lookup
+            if ra + rb == rc - rd:  # see if results hit our criteria
+                print(f"f({a}) + f({b}) = f({c}) - f({d})\t{ra} + {rb} = {rc} - {rd}")
+            return  # exit out of this call to prevent further loop
+
+        for n in src:
+            arr.append(n)
+            fn(src, arr)
+            arr.pop()
+
+    fn(q, [])
+
+
+print(f"\ntuple: {q1}\n")
+sumdiff(q1)
+print(f"\nset: {q2}\n")
+sumdiff(q2)
