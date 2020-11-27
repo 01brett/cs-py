fc3f3a32d2471fbf8372e91060dd7e29eb1ea3d9
brett f <1brettf@gmail.com>
2020-11-27 15:53:49 -0800

    Implement ex5

--- py25_sprint_chal/hashtables/ex5/ex5.py
+++ py25_sprint_chal/hashtables/ex5/ex5.py
@@ -1,25 +1,20 @@
 # Your code here
 
 
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    cache = {q: True for q in queries}
+
+    for file in files:
+        temp = file.split("/")
+        target = temp[-1]
+        if target in cache:
+            result.append(file)
 
     return result
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
+    files = ["/bin/foo", "/bin/bar", "/usr/bin/baz"]
+    queries = ["foo", "qux", "baz"]
     print(finder(files, queries))
