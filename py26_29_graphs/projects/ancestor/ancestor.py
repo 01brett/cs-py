43848626a42e4b52948ae916be742796fc53d966
brett f <1brettf@gmail.com>
2020-12-02 18:50:57 -0800

    Improve comments

--- py26_29_graphs/projects/ancestor/ancestor.py
+++ py26_29_graphs/projects/ancestor/ancestor.py
@@ -6,35 +6,28 @@ def get_parents(data, target):
 
         if child == target:
             parents.append(parent)
-
-        if len(parents) == 2:
+        if len(parents) == 2:  # only 2 parents possible, so break!
             break
 
     if len(parents) > 0:
-        parents.sort(reverse=True)
+        parents.sort(reverse=True)  # fixes where we have two earliest
         return parents
-
     return None
 
 
 def earliest_ancestor(ancestors, starting_node):
-    q = []
-    q.append(starting_node)
-
+    q = [starting_node]  # q is simple, no need for full-on Queue()
     ans = -1
 
     while len(q) > 0:
         node = q.pop(0)
-
         parents = get_parents(ancestors, node)
 
         if parents:
             for parent in parents:
                 q.append(parent)
-        else:
-            if node == starting_node:
+        else:  # get_parents -> None aka no parents
+            if node == starting_node:  # no ancestors! break da loop
                 break
-
-            ans = node
-
+            ans = node  # update our answer to the earliest so far
     return ans
